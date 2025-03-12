ddda2437cd82d4087f6f34c827fb703e60f381d2
Weicheng Hu <76918682+Oki3@users.noreply.github.com>
2025-03-12 20:53:19 +0100

    test if rules working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 module.exports = {
   rules: {
-    'energy-hotspot': require('./src/rules/energy-hotspot')
+    'energy-hotspot': require('./src/rules/energy-hotspot'),
+    'no-multiple-imgs': require('./src/rules/no-multiple-imgs')
   }
 };
