2c7613ef49618cb714e9c13201935a6cefb7772b
Weicheng Hu <76918682+Oki3@users.noreply.github.com>
2025-04-01 11:06:30 +0200

    Change autocomplete: instead of the autocomplete params, check for input fields

--- plugin/src/rules/avoid-autocomplete.js
+++ plugin/src/rules/avoid-autocomplete.js
@@ -4,7 +4,7 @@ module.exports = {
   meta: {
     type: 'suggestion',
     docs: {
-      description: 'Warns against using autocomplete on form fields',
+      description: 'Warns against using autocomplete components',
       recommended: true,
     },
     schema: [] // No options
@@ -12,87 +12,30 @@ module.exports = {
   create(context) {
     return {
       JSXOpeningElement(node) {
-        if (node.name.name === 'input') {
-          // Check if autocomplete is explicitly turned off
-          const autocompleteAttr = node.attributes.find(
-            attr => attr.type === 'JSXAttribute' && attr.name.name === 'autoComplete'
-          );
-          
-          if (!autocompleteAttr) {
-            context.report({
-              node,
-              message: 'Input field should have autoComplete="off"'
-            });
-          } else if (
-            autocompleteAttr.value &&
-            autocompleteAttr.value.type === 'Literal' &&
-            autocompleteAttr.value.value !== 'off'
-          ) {
-            context.report({
-              node,
-              message: 'Input field should have autoComplete="off"'
-            });
-          }
-        } else if (node.name.name === 'form') {
-          // Check form elements for autocomplete attribute
-          const autocompleteAttr = node.attributes.find(
-            attr => attr.type === 'JSXAttribute' && attr.name.name === 'autoComplete'
-          );
-          
-          if (!autocompleteAttr) {
-            context.report({
-              node,
-              message: 'Form should have autoComplete="off"'
-            });
-          } else if (
-            autocompleteAttr.value &&
-            autocompleteAttr.value.type === 'Literal' &&
-            autocompleteAttr.value.value !== 'off'
-          ) {
-            context.report({
-              node,
-              message: 'Form should have autoComplete="off"'
-            });
-          }
-        }
-      },
-      
-      // Check DOM API for input creation
-      CallExpression(node) {
-        // Check document.createElement('input')
-        if (
-          node.callee.type === 'MemberExpression' &&
-          node.callee.object.name === 'document' &&
-          node.callee.property.name === 'createElement' &&
-          node.arguments.length > 0 &&
-          node.arguments[0].type === 'Literal' &&
-          node.arguments[0].value === 'input'
-        ) {
+        if (node.name.name === 'Autocomplete') {
           context.report({
             node,
-            message: 'Remember to set autocomplete="off" on programmatically created input elements'
+            message: 'consider replacing Autocomplete component with a normal TextField or input element'
           });
         }
       },
       
-      // Check for setAttribute calls that might be setting autocomplete
-      MemberExpression(node) {
-        if (
-          node.property.name === 'setAttribute' &&
-          node.parent &&
-          node.parent.type === 'CallExpression' &&
-          node.parent.arguments.length >= 2 &&
-          node.parent.arguments[0].type === 'Literal' &&
-          node.parent.arguments[0].value === 'autocomplete' &&
-          (node.parent.arguments[1].type !== 'Literal' ||
-            node.parent.arguments[1].value !== 'off')
-        ) {
-          context.report({
-            node: node.parent,
-            message: 'Set autocomplete="off" for better energy usage'
-          });
-        }
-      }
+      // CallExpression(node) {
+      //   // Check document.createElement('input')
+      //   if (
+      //     node.callee.type === 'MemberExpression' &&
+      //     node.callee.object.name === 'document' &&
+      //     node.callee.property.name === 'createElement' &&
+      //     node.arguments.length > 0 &&
+      //     node.arguments[0].type === 'Literal' &&
+      //     node.arguments[0].value === 'input'
+      //   ) {
+      //     context.report({
+      //       node,
+      //       message: 'Remember to set autocomplete="off" on programmatically created input elements'
+      //     });
+      //   }
+      // },
     };
   }
 };
